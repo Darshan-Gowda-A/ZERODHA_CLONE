1a34e201f6e56e85d5d87cfd809a4809eba307bc
Darshan-Gowda-A <darshgowdru2004@gmail.com>
2026-02-21 17:33:20 +0530

    complete frontend done

---
+++ frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero";
+import RaceTicket from "./RaceTicket";
+
+function SupportPage() {
+  return (
+    <>
+      <Hero />
+      <RaceTicket />
+    </>
+  );
+}
+export default SupportPage;
